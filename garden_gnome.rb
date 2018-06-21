d713d5d674f72780c797b69e7aa4bbc7fa1adba5
emmagraves <emma.graves09@gmail.com>
2018-06-21 21:10:53 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,17 +1,18 @@
 # Code your instances here
 
 class GardenGnome
-  attr_accessor :name ,:age ,:gluten_allergy
-  def initialize(name,age,gluten_allergy,)
+  attr_accessor :name ,:age ,:gluten_allergy ,:personality 
+  def initialize(name,age,gluten_allergy,personality)
     @name = name 
     @age = age 
   @gluten_allergy = gluten_allergy
+  @personality = personality
   end 
   
 end 
 
 GardenGnome1 = GardenGnome.new("Paige", 4000) 
 GardenGnome2 = GardenGnome.new("Emma", 4000)
-
-
+ 
+puts @personality = "evil"
 
